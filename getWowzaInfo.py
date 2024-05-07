035e65ea88ef0783952d4f22bc19cecbfbbae739
RocketWebcast <135097798+RocketWebcast@users.noreply.github.com>
2024-05-07 16:17:07 +0000

    getWowzaInfo.py
    New script using urllib3 and also adding some properties like uptime, bytes in/out ect.

--- getWowzaInfo.py
+++ getWowzaInfo.py
@@ -1,9 +1,14 @@
 #!/bin/env python
-# Vicente Dominguez
+# by Mike Schaefer based on Vicente Dominguez
 # -a conn - Global connections
 # -a appnum - Global application(streaming) num
+# -a uptime - Global uptime
+# -a conntotal - Global total connections
+# -a inbytes - Global incoming bytes
+# -a outbytes - Global outgoing bytes
+# -a all - All the above in pipe format
 
-import urllib2, base64, sys, getopt
+import urllib.request, urllib3, base64, sys, getopt
 import xml.etree.ElementTree as ET
 
 # Default values
@@ -11,24 +16,38 @@ username = "admin"
 password = "admin"
 host = "localhost"
 port = "8086"
-realm = "Wowza Media Systems"
 getInfo = "None"
 
 ##
 
 def Usage ():
-        print "Usage: getWowzaInfo.py -u user -p password -h 127.0.0.1 -P 8086 -a [conn|appnum]"
+        print("Usage: getWowzaInfo.py -u user -p password -h 127.0.0.1 -P 8086 -a [conn|appnum|uptime|conntotal|inbytes|outbytes|all]")
         sys.exit(2)
 
 def getCurrentConnections():
-        print xmlroot[0].text
+        print(xmlroot[0].text)
+
+def getTotalConnections():
+        print(xmlroot[1].text)
 
 def getCurrentStreams():
         Application =  xmlroot.findall('VHost/Application')
-        print len(Application)
+        print(len(Application))
+		
+def getUptime():
+        print(xmlroot.find('VHost').find('TimeRunning').text)
+
+def getInbytes():
+        print(xmlroot.find('MessagesInBytesRate').text)
+
+def getOutbytes():
+        print(xmlroot.find('MessagesOutBytesRate').text)
+
+def getAll():
+	    print(xmlroot.find('VHost').find('TimeRunning').text + "|" + xmlroot.find('MessagesOutBytesRate').text + "|" + xmlroot.find('MessagesInBytesRate').text + "|" + xmlroot[0].text + "|" + xmlroot[1].text + "|" + str(len(xmlroot.findall('VHost/Application'))))
 
 def unknown():
-        print "unknown"
+        print("unknown")
 
 ##
 
@@ -53,7 +72,7 @@ def main (username,password,host,port,getInfo):
 					if opt == "-h" :
 							host = arg
 					if opt == "-P" :
-							port = arg
+							port = str(arg)
 					if opt == "-a" :
 							getInfo = arg
 	except :
@@ -61,22 +80,40 @@ def main (username,password,host,port,getInfo):
 
 
 	url="http://" + host + ":" + port + "/connectioncounts/"
-	authhandler = urllib2.HTTPDigestAuthHandler()
-	authhandler.add_password(realm, url, username, password)
-	opener = urllib2.build_opener(authhandler)
-	urllib2.install_opener(opener)
-	page_content = urllib2.urlopen(url)
-	xmlroot = ET.fromstring(page_content.read())	
-
+ 
+	http = urllib3.PoolManager()
+	http.cache = True
+	http.cache_timeout = 30
+	http.cache_maxsize = 1000
+	http.cache_block = True
+	http.cache_storage = True
+	http.cache_content = True
+	myHeaders = urllib3.util.make_headers(basic_auth=username + ":" + password)
+	myHeaders["Authorization"] = "Basic %s" % base64.b64encode(bytes('%s:%s' % (username, password), 'utf-8')).decode('utf-8')
+	myHeaders["Content-Type"] = "application/xml"
+	myHeaders["Accept"] = "application/xml"
+	myHeaders["Cache-Control"] = "public, min-fresh=20, max-age=30 immutable"
+	result = http.request('GET', url, headers=myHeaders, timeout=5.0)
+	xmlroot = ET.fromstring(result.data.decode('ascii'))
+	# print(xmlroot.tag)
 
 
 	if ( getInfo == "conn"):
 			getCurrentConnections()
+	elif ( getInfo == "conntotal"):
+			getTotalConnections()
 	elif ( getInfo == "appnum"):
 			getCurrentStreams()
+	elif ( getInfo == "uptime"):
+			getUptime()
+	elif ( getInfo == "inbytes"):
+			getInbytes()
+	elif ( getInfo == "outbytes"):
+			getOutbytes()
+	elif ( getInfo == "all"):
+			getAll()
 	else:
-			unknown()
-			sys.exit(1)
+			getAll()
 
 
 
